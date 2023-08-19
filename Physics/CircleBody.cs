4b91d5683956b580d8d59d99166cb173620212dd
Modleyyy <zaki.amarouche.tralala@gmail.com>
2023-08-19 19:06:07 +0100

    Replaced the if statements by a switch statement, and added default value for DrawMode

--- Physics/CircleBody.cs
+++ Physics/CircleBody.cs
@@ -11,13 +11,14 @@ public class CircleBody : PhysicsBody
 
     public override void Draw(Graphics graphics, Color color, DrawMode drawMode = DrawMode.Border)
     {
-        if (drawMode == DrawMode.Border)
+        switch (drawMode)
         {
-            graphics.DrawEllipse(new Pen(color), center.x - radius, center.y - radius, radius*2, radius*2);
-        }
-        else if (drawMode == DrawMode.Fill)
-        {
-            graphics.FillEllipse(new SolidBrush(color), center.x - radius, center.y - radius, radius*2, radius*2);
+            case DrawMode.Border:
+                graphics.DrawEllipse(new Pen(color), center.x - radius, center.y - radius, radius*2, radius*2);
+                break;
+            case DrawMode.Fill:
+                graphics.FillEllipse(new SolidBrush(color), center.x - radius, center.y - radius, radius*2, radius*2);
+                break;
         }
     }
 }
